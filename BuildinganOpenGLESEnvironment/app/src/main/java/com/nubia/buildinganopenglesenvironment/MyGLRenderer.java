ab37388d570abe699d6ca74ece9c16815e73b479
LiuYongfeng <lyf198511@163.com>
2017-09-11 18:41:24 +0800

    Building an OpenGL ES Environment add zhushi

--- BuildinganOpenGLESEnvironment/app/src/main/java/com/nubia/buildinganopenglesenvironment/MyGLRenderer.java
+++ BuildinganOpenGLESEnvironment/app/src/main/java/com/nubia/buildinganopenglesenvironment/MyGLRenderer.java
@@ -11,17 +11,17 @@ import javax.microedition.khronos.opengles.GL10;
  */
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
-
+    //Called once to set up the view's OpenGL ES environment.
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
-
+    //Called for each redraw of the view.
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
     }
-
+   //Called if the geometry of the view changes, for example when the device's screen orientation changes.
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
     }
--- BuildinganOpenGLESEnvironment/app/src/main/java/com/nubia/buildinganopenglesenvironment/OpenGLES20Activity.java
+++ BuildinganOpenGLESEnvironment/app/src/main/java/com/nubia/buildinganopenglesenvironment/OpenGLES20Activity.java
@@ -8,6 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by admin on 2017-9-11.
+ * A GLSurfaceView is a view container for graphics drawn with OpenGL
+ * GLSurfaceView.Renderer controls what is drawn within that view.
+ * This lesson explains how to complete a minimal implementation of GLSurfaceView and
+ * GLSurfaceView.Renderer in a simple application activity.
  */
 
 public class OpenGLES20Activity extends AppCompatActivity {
@@ -17,6 +21,8 @@ public class OpenGLES20Activity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
         mGLView = new MyGLSurfaceView(this);
         setContentView(mGLView);
     }
@@ -26,7 +32,9 @@ public class OpenGLES20Activity extends AppCompatActivity {
         private final MyGLRenderer mRenderer;
         public MyGLSurfaceView(Context context){
             super(context);
+            // Create an OpenGL ES 2.0 context
             setEGLContextClientVersion(2);
+            // Set the Renderer for drawing on the GLSurfaceView
             mRenderer = new MyGLRenderer();
             setRenderer(mRenderer);
         }
